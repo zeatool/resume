878e17d35a1729d1e3d4c2dab47d4de9ec25ae34
Baranov-rp <baranov-rp@biryusa.ru>
2016-05-06 16:09:00 +0700

    Доработки

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 $(function () {
    // tooltips
     $('[data-toggle="tooltip"]').tooltip();
+
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
 });
